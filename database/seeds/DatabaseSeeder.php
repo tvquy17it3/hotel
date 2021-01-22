41986803a0e45568a4389eb23d60759b21eb980f
vvnha <vvnha.17it3@sict.dn.vn>
2021-01-23 00:21:30 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,9 +25,6 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Deluxe Twin Ocean View', 'number' => '204', 'img'=> '','kindOfRooms' => '2', 'price'=>1417500, 'capacity' => '4', 'status' => '0' ],
          ]);
          DB::table('users')->insert([
-            ['name' => 'Nhã', 'email' => 'vvnha.17it3@sict.udn.vn', 'phone'=> '0905905905', 'email_verified_at'=>'', 'password' => '$2y$10$0eCGHlUcbjJeVGVrDkYDcuCDKyiv4s7I4go3cT2IKjYHIEZ.XhQJu', 'role' => '1', 'remember_token'=>'', 'created_at'=>'', 'updated_at'=>'']
-         ]);
-         DB::table('users')->insert([
             ['name' => 'Võ Văn Nhã','email' => 'vvnha.17it3@sict.udn.vn','phone' => '0905903902', 'email_verified_at'=>'', 'password'=>'$2y$10$iNvCL3cT.jJAomB5a.8eEOFsfxU1eeKPNpH3eWdxd83C.AJRCFys.', 'role' => '1', 'remember_token'=>'', 'created_at'=>'', 'updated_at'=>''],
             ['name' => 'Trần Văn Quý','email' => 'vanquy@gmail.com','phone' => '0905906666', 'email_verified_at'=>'', 'password'=>'$2y$10$iNvCL3cT.jJAomB5a.8eEOFsfxU1eeKPNpH3eWdxd83C.AJRCFys.', 'role' => '1', 'remember_token'=>'', 'created_at'=>'', 'updated_at'=>''],
             ['name' => 'Nguyễn Văn Tuyên','email' => 'tuyen1@gmail.com','phone' => '0905903902', 'email_verified_at'=>'', 'password'=>'$2y$10$iNvCL3cT.jJAomB5a.8eEOFsfxU1eeKPNpH3eWdxd83C.AJRCFys.', 'role' => '1', 'remember_token'=>'', 'created_at'=>'', 'updated_at'=>'']
