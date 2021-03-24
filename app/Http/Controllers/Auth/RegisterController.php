a9b22515cf97163d32f651b5de27ad0c49c1445f
tvquy17it3 <38048486+tvquy17it3@users.noreply.github.com>
2021-03-24 15:39:43 +0700

    up

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Providers\RouteServiceProvider;
 
 class RegisterController extends Controller
 {
